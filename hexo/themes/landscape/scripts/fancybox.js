728383cd637d98bb27fa414ef9e507543936c62b
nspangbo <wangbo.nspangbo@bytedance.com>
2022-04-19 09:39:28 +0800

    update landscape theme

--- hexo/themes/landscape/scripts/fancybox.js
+++ hexo/themes/landscape/scripts/fancybox.js
@@ -17,8 +17,8 @@ hexo.extend.tag.register('fancybox', function(args){
 
   var title = args.join(' ');
 
-  return '<a class="fancybox" href="' + original + '" title="' + title + '">' +
-    '<img src="' + (thumbnail || original) + '" alt="' + title + '">'
-    '</a>' +
-    (title ? '<span class="caption">' + title + '</span>' : '');
+  return `<a data-fancybox="gallery" href="${original}" data-caption="${title}">
+    <img src="${thumbnail || original}" alt="${title}">
+    </a>
+    ${title ? `<span class="caption">${title}</span>` : ''}`;
 });
--- hexo/themes/landscape/source/js/script.js
+++ hexo/themes/landscape/source/js/script.js
@@ -41,6 +41,7 @@
       url = $this.attr('data-url'),
       encodedUrl = encodeURIComponent(url),
       id = 'article-share-box-' + $this.attr('data-id'),
+      title = $this.attr('data-title'),
       offset = $this.offset();
 
     if ($('#' + id).length){
@@ -55,10 +56,10 @@
         '<div id="' + id + '" class="article-share-box">',
           '<input class="article-share-input" value="' + url + '">',
           '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
             '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
             '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"></a>',
           '</div>',
         '</div>'
       ].join('');
@@ -88,13 +89,13 @@
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
+      if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
 
       var alt = this.alt;
 
       if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
     });
 
     $(this).find('.fancybox').each(function(){
